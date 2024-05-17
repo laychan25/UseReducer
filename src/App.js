3fa33e8146670d2c04aeb18fe8b4dd39f1818702
laychan25 <lauriesilva0@gmail.com>
2024-05-17 15:25:48 -0300

    comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer, useState } from "react";
+import reducer, { ADICIONAR_FRASE } from "./reducer";
+import { EXCLUIR_FRASE } from "./reducer";
+
+
+
+
 
 function App() {
+  const [frase, setfrase] = useState("");
+  
+ const[frases , dispatch] =useReducer(reducer, [])
+
+  function salvarFrase(evento) {
+    evento.preventDefault();
+   
+    dispatch({
+      tipo: ADICIONAR_FRASE,
+      frase
+    })
+  }
+
+  function excluir (fraseExcluida){
+    dispatch({
+      tipo: EXCLUIR_FRASE,
+      frase: fraseExcluida
+    })
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     
+
+      <form onSubmit={salvarFrase}>
+        <textarea
+          onChange={(evento) => setfrase(evento.target.value)}
+          placeholder="Digite sua frase"
+          required
+        />
+        <br />
+        <button>Salvar Frase</button>
+      </form>
+      {frases.map((fraseAtual, index) => ( 
+      <div  key={index}>
+            <p>{fraseAtual}
+            <button onClick={()=>excluir(fraseAtual)}>Excluir
+            </button>
+            </p>
+         </div>
+      ))}
+      
     </div>
   );
 }
